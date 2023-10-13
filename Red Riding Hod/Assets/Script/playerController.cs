dc91fdb98896a9b6d70e1236657c650ef029dcd3
farchanul <94830919+farchanul@users.noreply.github.com>
2023-10-13 21:34:41 +0700

    Revert "marge branch mba dwi sama wave"
    This reverts commit 5e9bcdf3f4aca4ef57f37202350661e828b5ced1.

--- Red Riding Hod/Assets/Script/playerController.cs
+++ Red Riding Hod/Assets/Script/playerController.cs
@@ -18,21 +18,12 @@ public class playerController : MonoBehaviour
 
     public Transform scytheSp;
     public GameObject scythePv;
-    //audio
-    private AudioManager audioManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        //audio
-        audioManager = AudioManager.instance;
-        if (audioManager == null)
-        {
-            Debug.LogWarning("audio manager di temukan");
-        }
-
         isAttack = false;
-        isFacing = false;
+        isFacing = false;  
         bulletForce = 15f;
         recoil = 6f;
         rbPlayer = GameObject.Find("player").GetComponent<Rigidbody>();
@@ -52,62 +43,51 @@ public class playerController : MonoBehaviour
         Facing();
     }
     // fungsi player Attak
-    void playerAttack()
-    {
-        audioManager.PlaySound("Shoot");
-        if (Input.GetMouseButtonDown(0))
-        { // mengetahui jika mouse di tekan ke bawah
+    void playerAttack(){
+        if(Input.GetMouseButtonDown(0)){ // mengetahui jika mouse di tekan ke bawah
             isAttack = true;
         }
-        if (isAttack)
-        {
-            for (int i = -2; i < 2; i++)
-            {
-                var bullet = Instantiate(bulletPV, bulletSP.position, bulletSP.rotation);//belum jadi
+        if (isAttack){
+            for (int i = -2; i<2;i++){
+                var bullet = Instantiate(bulletPV,bulletSP.position,bulletSP.rotation);//belum jadi
                 Vector3 rotation = mousePosition - rotpoint.position;//variabel untuk membuat rotasi (diambil dari mouse position dikurang object position(object rotasi))
-                float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;//variabel untuk membuat derajat rotasi(diambil dari Tangen(variabel rotation(y,x))selain sumbu rotasi)
-                bullet.transform.rotation = Quaternion.Euler(180 + rotz + (3f * i), 270, 0);
+                float rotz = Mathf.Atan2(rotation.y,rotation.x) * Mathf.Rad2Deg;//variabel untuk membuat derajat rotasi(diambil dari Tangen(variabel rotation(y,x))selain sumbu rotasi)
+                bullet.transform.rotation = Quaternion.Euler(180+rotz+(3f*i),270,0);
                 bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletForce;
             }
             PlayerMove(mousePosition);
-            var Scythe = Instantiate(scythePv, scytheSp.position, Quaternion.identity);
+            var Scythe = Instantiate(scythePv,scytheSp.position,Quaternion.identity);
             isAttack = false;
         }
     }
-    // fungsi mengambil posisi mouse
-    void getMousePosition()
-    {
+// fungsi mengambil posisi mouse
+    void getMousePosition(){
         var mousePos = Input.mousePosition;
-        mousePos.z = 13;
+        mousePos.z=13;
         mousePosition = cam.ScreenToWorldPoint(mousePos);
         trMousePos.position = mousePosition;
     }
     //fungsi menggerakan player
-    void PlayerMove(Vector3 mousePos)
-    {
-        Vector3 recoilPos;
-        recoilPos = mousePos - gameObject.transform.position; // menentukan arah mousePos
-        recoilPos = new Vector3(recoilPos.x, recoilPos.y, recoilPos.z).normalized; // membuat posisi recoilPos menjadi antara -1 sampai 1
-        rbPlayer.AddForce(new Vector3(recoilPos.x, recoilPos.y, 0) * -recoil, ForceMode.VelocityChange); // memberi gaya recoilPos pada object
+    void PlayerMove(Vector3 mousePos){
+            Vector3 recoilPos;
+            recoilPos = mousePos - gameObject.transform.position; // menentukan arah mousePos
+            recoilPos = new Vector3(recoilPos.x,recoilPos.y,recoilPos.z).normalized; // membuat posisi recoilPos menjadi antara -1 sampai 1
+            rbPlayer.AddForce(new Vector3(recoilPos.x,recoilPos.y,0) * -recoil,ForceMode.VelocityChange); // memberi gaya recoilPos pada object
     }
     // fungsi rotasi senjata
-    void rotateWepon()
-    {
+    void rotateWepon(){
         Vector3 rotation = mousePosition - rotpoint.position;//variabel untuk membuat rotasi (diambil dari mouse position dikurang object position(object rotasi))
-        float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;//variabel untuk membuat derajat rotasi(diambil dari Tangen(variabel rotation(y,x))selain sumbu rotasi)
-        rotpoint.transform.rotation = Quaternion.Euler(0, 0, rotz);//membuat rotasi objek sesuai sudud variable rotz
+        float rotz = Mathf.Atan2(rotation.y,rotation.x) * Mathf.Rad2Deg;//variabel untuk membuat derajat rotasi(diambil dari Tangen(variabel rotation(y,x))selain sumbu rotasi)
+        rotpoint.transform.rotation = Quaternion.Euler(0,0,rotz);//membuat rotasi objek sesuai sudud variable rotz
     }
     // facing
-    void Facing()
-    {
-        if (mousePosition.x - transform.position.x < 0 && !isFacing)
-        {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
+    void Facing(){
+        if(mousePosition.x - transform.position.x < 0 && !isFacing){
+            transform.rotation = Quaternion.Euler(0,180,0);
             isFacing = true;
         }
-        if (mousePosition.x - transform.position.x > 0 && isFacing)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+        if(mousePosition.x - transform.position.x > 0 && isFacing){
+            transform.rotation = Quaternion.Euler(0,0,0);
             isFacing = false;
         }
     }
