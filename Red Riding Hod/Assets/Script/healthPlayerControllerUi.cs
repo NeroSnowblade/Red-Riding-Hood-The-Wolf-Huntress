7a5c2dcaac066e958c53fbfd3fb1518bf5f4a835
farchanul <94830919+farchanul@users.noreply.github.com>
2023-10-17 21:47:58 +0700

    penggabungan dengan projek mas daniel
    health bar ui controller

--- Red Riding Hod/Assets/Script/healthPlayerControllerUi.cs
+++ Red Riding Hod/Assets/Script/healthPlayerControllerUi.cs
@@ -5,16 +5,35 @@ using UnityEngine.UI;
 
 public class healthPlayerControllerUi : MonoBehaviour
 {
-    public Slider slider;
+    // public Slider slider;
 
-    public void setMaxHealth(float maxHealth)
-    {
-        slider.maxValue = maxHealth;
-        slider.value = maxHealth;
-    }
+    // public void setMaxHealth(float maxHealth)
+    // {
+    //     slider.maxValue = maxHealth;
+    //     slider.value = maxHealth;
+    // }
+
+    // public void setHealth(float health)
+    // {
+    //     slider.value = health;
+    // }
+    public Slider slider;
+	public Gradient gradient;
+	public Image fill;
+
+	public void setMaxHealth(float health)
+	{
+		slider.maxValue = health;
+		slider.value = health;
+
+		fill.color = gradient.Evaluate(1f);
+	}
 
     public void setHealth(float health)
-    {
-        slider.value = health;
-    }
+	{
+		slider.value = health;
+
+		fill.color = gradient.Evaluate(slider.normalizedValue);
+	}
+
 }
